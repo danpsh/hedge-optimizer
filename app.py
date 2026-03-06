24c6c671f4465b0c612c3f0dab2a2d95b915bcb2
danpsh <danielpshonyak@gmail.com>
2026-03-06 10:22:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,9 +69,8 @@ with st.container():
             "NCAAW": "basketball_wncaab",
             "NHL": "icehockey_nhl",
             "Boxing": "boxing_boxing",
-            "MMA": "mma_mixed_martial_arts 
+            "MMA": "mma_mixed_martial_arts" 
         }
-        
         sport_labels = list(sports_map.keys())
         selected_sports = []
 
@@ -237,6 +236,7 @@ with st.expander("Open Manual Calculator", expanded=False):
                 rc3.metric("ROI", f"{((m_p/mw)*100):.1f}%")
             except: 
                 st.error("Please enter valid numbers.")
+
 
 
 
